0746b360bd16b8227039d62d975df0ba1814fd7d
YutaSugao <mrfeather@YutaSugaonoMacBook.local>
2018-04-15 12:16:58 +0900

    User Admin Seed add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Admin.create(:admin_name =>"管理人A", :admin_email =>"kanri1@cp.com", :admin_password =>"123456")
-Admin.create(:admin_name =>"管理人B", :admin_email =>"kanri2@cp.com", :admin_password =>"123456")
